acd99f5f62cdd9a4bcd395e51d393d0bb811689e
Dhiksa Raja Nararya Hakim <tshiba257@gmail.com>
2025-05-19 12:18:39 +0700

    dhiksa

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,7 +12,7 @@
         <!-- Sidebar -->
         <aside class="w-64 bg-white shadow-md hidden md:block">
             <div class="p-6 text-center font-bold text-lg border-b">Admin Panel</div>
-             <nav class="p-4 space-y-4">
+            <nav class="p-4 space-y-4">
                 <a href="{{ route('admin-dashboard') }}" class="block px-4 py-2 rounded hover:bg-gray-200 {{ request()->routeIs('admin-dashboard') ? 'bg-gray-100 font-semibold' : '' }}">
                     Dashboard
                 </a>
