dc94ab2b3b412505fa3bebf1c3d63d77badce9c4
waxpoo <muhammadruhulbayan.mrb@gmail.com>
2025-05-27 10:50:37 +0700

    a

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,9 +11,15 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    // Kolom yang bisa diisi massal
     protected $fillable = [
         'name',
+        'email',
         'password',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'alamat',
+        'kelas',
         'level',
     ];
 
@@ -24,5 +30,11 @@ class User extends Authenticatable
 
     protected $casts = [
         'password' => 'hashed',
+        'tanggal_lahir' => 'date',
     ];
+
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'guru_id');
+    }
 }
--- app/Models/student.php
+++ app/Models/student.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Student extends Model
 {
     protected $fillable = [
-        'user_id',
         'guru_id',
         'name',
         'password',
